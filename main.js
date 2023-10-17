3a04dd626c1ae2719d96c231d26145661ab851aa
rmotti <rmotti99@gmail.com>
2023-10-16 21:37:26 -0300

    feat: aula 3

--- main.js
+++ main.js
@@ -28,3 +28,33 @@ window.addEventListener("scroll", function () {
     header.classList.remove("scroll");
   }
 });
+
+/* Testimonials slider swiper*/
+
+const swiper = new Swiper('.swiper-container',{
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+});
+
+/* ScrollReveal: mostrar elementos ao dar scroll na pagina*/
+
+const scrollReveal = ScrollReveal ({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services .header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links
+  `, {interval : 100})
+
+
